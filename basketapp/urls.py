3b4587618c47f1c07dc9ebfaf1fe0ab6f3c8fe8a
kostitsyn <6YUJDKlose3830overLOAD>
2020-11-10 13:52:46 +0400

    Шестое домашнее задание

--- basketapp/urls.py
+++ basketapp/urls.py
@@ -8,7 +8,6 @@ urlpatterns = [
     path('', basketapp.BasketListView.as_view(), name='basket'),
 
     path('add/<pk>/', basketapp.basket_add, name='add'),
-    path('add/<pk>/', basketapp.basket_add, name='add'),
 
     # path('remove/<pk>/', basketapp.basket_remove, name='remove'),
     path('remove/<pk>/', basketapp.BasketDeleteView.as_view(), name='remove'),
