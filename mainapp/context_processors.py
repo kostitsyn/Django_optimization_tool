c199942072fe7c0b799f6544d899e20c2950b380
kostitsyn <6YUJDKlose3830overLOAD>
2020-11-10 13:14:13 +0400

    Шестое домашнее задание

--- mainapp/context_processors.py
+++ mainapp/context_processors.py
@@ -5,7 +5,7 @@ def get_basket(request):
     basket_items = []
 
     if request.user.is_authenticated:
-        basket_items = Basket.objects.filter(user=request.user)
+        basket_items = Basket.objects.filter(user=request.user).select_related('product')
 
     return {
         'basket': basket_items
