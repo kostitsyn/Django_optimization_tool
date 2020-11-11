2e5234716bbe22ece39752bf7252985a220b9ce5
kostitsyn <6YUJDKlose3830overLOAD>
2020-11-11 14:40:49 +0400

    Шестое домашнее задание

--- ordersapp/views.py
+++ ordersapp/views.py
@@ -134,12 +134,6 @@ class OrderItemsDeleteView(DeleteView):
 class OrderItemsReadView(DetailView):
     model = Order
 
-    def get_object(self, queryset=None):
-        a = super().get_object(queryset=None)
-        print('hello')
-        print(a)
-        return a
-
     def get_context_data(self, **kwargs):
         context_data = super(OrderItemsReadView, self).get_context_data(**kwargs)
         context_data['title'] = 'просмотр заказа'
