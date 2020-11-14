2ac393e3f90f1e7c0b7b6861f4010df1eaed0ace
kostitsyn <6YUJDKlose3830overLOAD>
2020-11-14 13:31:26 +0400

    Седьмое домашнее задание

--- ordersapp/views.py
+++ ordersapp/views.py
@@ -51,7 +51,8 @@ class OrderItemsCreateView(CreateView):
         if self.request.POST:
             formset = OrderFormSet(self.request.POST)
         else:
-            basket_item = Basket.get_items(self.request.user)
+            # basket_item = Basket.get_items(self.request.user)
+            basket_item = Basket.get_items_cached
 
             if len(basket_item):
                 OrderFormSet = inlineformset_factory(Order, OrderItem, form=OrderItemForm, extra=len(basket_item))
