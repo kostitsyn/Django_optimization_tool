256ad9f92b64362356b3ac4ad4277b02c122371a
kostitsyn <6YUJDKlose3830overLOAD>
2020-11-14 12:00:25 +0400

    Седьмое домашнее задание

--- static/js/main_scripts.js
+++ static/js/main_scripts.js
@@ -4,7 +4,7 @@ $(document).on('click', '.details a', function(event) {
     if (event.target.hasAttribute('href')) {
         let link = event.target.href + 'ajax/';
         let link_array = link.split('/')
-        if (link_array[3] == 'category') {
+        if (link_array[4] == 'category') {
             $.ajax({
                 url: link,
                 success: function(data) {
